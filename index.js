1a5ea664c897baac1d540196cf1821817b937c5c
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:57:50 -0800

    Alien Color 2
    Typescript 45Q assignment exercise 26

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+// First version (Run the if block)
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log('congratulation! You just earned 5 points for shooting the alien.');
+}
+else {
+    console.log('congratulation! You just earned 10 points for shooting the alien.');
+}
+// version 2 (run the else block)
+console.log(`2nd version of code`);
+alien_color = 'blue';
+if (alien_color === 'green') {
+    console.log('congratulation! You just earned 5 points for shooting the alien.');
+}
+else {
+    console.log('congratulation! You just earned 10 points for shooting the alien.');
+}
